a3947d1adcc14677ed4c0e0ea81732604b15889d
sabrin-05 <11sabrin10@gmail.com>
2025-10-19 16:00:38 +0100

    Added comments to explain old project

--- ExtraProjects/CollectionManager/MainCode.py
+++ ExtraProjects/CollectionManager/MainCode.py
@@ -1,3 +1,11 @@
+'''Using an old project to implement unit testing techniques learnt at the start of DAT5501 module
+
+What is this project:
+A Movies/TVShows collection manager that has the ability to view collection items, both individually and in bulk.
+This project gives the user the ability to add additional items, remove them, and edit information about the item.'''
+
+
+
 class Watch_list:
     def __init__(self,movies_list=[],shows_list=[]):
         self.movies_list = movies_list
