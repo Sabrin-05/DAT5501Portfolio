fb4e1b3385649974f4d4cf4c942a2921f1809a54
sabrin-05 <11sabrin10@gmail.com>
2025-11-10 12:54:02 +0000

    Made code more efficient by removing for loop and using .diff()

--- Week8/PriceSortingActivity.py
+++ Week8/PriceSortingActivity.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from time import perf_counter
+import matplotlib.pyplot as plt 
 
  # Calculating the daily change in assest price
 
@@ -8,23 +9,23 @@ from time import perf_counter
 pricing_df = pd.read_csv("Week8/NVIDIA CORPORATION (03-20-2024 _ 11-07-2025).csv")
 print(pricing_df)
 
-#pricing_df['Price Change'] = pricing_df['Close'].diff()
-#print(pricing_df[['Date', 'Close', 'Price Change']])
+pricing_df['Price Change'] = pricing_df['Close'].diff()
+print(pricing_df[['Date', 'Close', 'Price Change']])
 
-changes = []
+pricing_df = np.array(0.0)
 
-
-for i in range(len(pricing_df['Close']) -1):
+'''for i in range(len(pricing_df['Close']) -1):
     start_price = pricing_df['Close'][i]
     next_price = pricing_df['Close'][i + 1]
     change_price = next_price - start_price
     changes.append(change_price)
 
 pricing_df['Price Change'] = change_price
-print(pricing_df[['Date', 'Close', 'Price Change']])
+print(pricing_df[['Date', 'Close', 'Price Change']])'''
 
-#start = perf_counter()
-
+start_time = perf_counter()
+subset_sample = pricing_df[0:8].sort()
+end_time = perf_counter()
 
 
 
