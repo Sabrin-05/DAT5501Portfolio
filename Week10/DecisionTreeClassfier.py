74a1d31593230868e37b28ee6d6ed834d851072d
sabrin-05 <11sabrin10@gmail.com>
2025-11-24 18:46:23 +0000

    Added a test train split on my x and y vaules from dataset

--- Week10/DecisionTreeClassfier.py
+++ Week10/DecisionTreeClassfier.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 from ucimlrepo import fetch_ucirepo 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,3 +20,11 @@ print(tic_tac_toe_endgame.metadata)
   
 # variable information 
 print(tic_tac_toe_endgame.variables) 
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Test Train Split: 70/30 split
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
