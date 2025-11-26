ef48cda8d0779bbb42b8975b4be68d87ab1bc16b
sabrin-05 <11sabrin10@gmail.com>
2025-11-26 12:32:54 +0000

    Oraganised presentation graph and removed unnecessary csv files

--- Week 3/PresentationGraph.py
+++ Week 3/PresentationGraph.py
@@ -1,42 +1,43 @@
+#Import necessary libraries
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Read and clean data from csv
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # Fetch the data.
-retirement_df = pd.read_csv("Week 3/sabrins_table(in).csv")
+retirement_df = pd.read_csv("Week 3/RetirementAgeData.csv")
 print(retirement_df)
 
-#retirment_df = retirement_df.mask(retirement_df["Entity"] !=  "OECD average")
-#clean_retirement_df = retirement_df.mask(retirement_df["Entity"] != "Europe average")
-#print(clean_retirement_df)
+#Masking continents and other averages
+retirment_df = retirement_df.mask(retirement_df["Entity"] !=  "OECD average")
+clean_retirement_df = retirement_df.mask(retirement_df["Entity"] != "Europe average")
+print(clean_retirement_df)
 
-#yearly_average= clean_retirement_df.groupby("Year")[["Average effective age of retirement, women (OECD)", "Average effective age of retirement, men (OECD)"]].mean().reset_index()
-#print(yearly_average)
+#Creating yealy average retirement age for men and women, grouping by year then calculating the mean
+yearly_average= clean_retirement_df.groupby("Year")[["Average effective age of retirement, women (OECD)", "Average effective age of retirement, men (OECD)"]].mean().reset_index()
+print(yearly_average)
 
-#for x in retirement_df.index():
-    #if df.loc[x, "Entity"] == "OECD average":
-        #retirement_df.drop(x, inplace=True)
 
-#used two axes as the scales were so different 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Graph 1: Show the average retirement age in men and women over 48yrs
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#Average retirement age for women
 fig, ax = plt.subplots()
-
-ax.scatter(retirement_df['year'], retirement_df['average_age_women'])
+ax.scatter(yearly_average['Year'], yearly_average['Average effective age of retirement, women (OECD)'])
 ax.set_xlabel("Year")
-#ax.set_ylabel('Average women retirement age', color='blue')
-#ax1.tick_params(axis='y', labelcolor='blue')
 
-# Create a second y-axis for the second line
-#ax2 = ax1.twinx()
-ax.scatter(retirement_df['year'], retirement_df['average_age_men'])
+#Average retirement age for men
+ax.scatter(yearly_average['Year'], yearly_average['Average effective age of retirement, men (OECD)'])
 ax.set_ylabel("Average Age")
-#ax.set_ylabel('Average men retirement age', color='red')
-#ax2.tick_params(axis='y', labelcolor='red')
 
+#Create graph
 plt.title('Global Retirement Age Trends by Gender (1970–2018)')
 plt.legend(["Average women retirement age","Average men retirement age"])
-
 plt.savefig('Correlation.png', bbox_inches= 'tight')
-
-# Show the plot
+#Show the plot
 plt.show()
